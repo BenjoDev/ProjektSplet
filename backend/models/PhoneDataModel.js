aa5cafc3efe7667924c46e6028777712067aaf1c
z-seni <35400507+z-seni@users.noreply.github.com>
2023-05-22 20:15:42 +0200

    updated data model for new structure

--- backend/models/PhoneDataModel.js
+++ backend/models/PhoneDataModel.js
@@ -2,24 +2,29 @@ var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
 var PhoneDataSchema = new Schema({
-	'capturedBy' : {
-		type: Schema.Types.ObjectId,
-		ref: 'user'
-   	},
-	'latitude' : Number,
-	'longitude' : Number,
+	// 'capturedBy' : {
+	// 	type: Schema.Types.ObjectId,
+	// 	ref: 'user'
+   	// },
+	'capturedBy' : Number,
+	'latitude_start' : Number,
+	'longitude_start' : Number,
+	'latitude_end' : Number,
+	'longitude_end' : Number,
 	'captureDate' : Date,
-	'accelerometerX' : Number,
-	'accelerometerY' : Number,
-	'accelerometerZ' : Number,
-	'userAccelerometerX' : Number,
-	'userAccelerometerY' : Number,
-	'userAccelerometerZ' : Number,
-	'gyroscopeX' : Number,
-	'gyroscopeY' : Number,
-	'gyroscopeZ' : Number,
-	'lightIntensity' : Number
+	'accelerometerX': [Number],
+	'accelerometerY': [Number],
+	'accelerometerZ': [Number],
+	'userAccelerometerX': [Number],
+	'userAccelerometerY': [Number],
+	'userAccelerometerZ': [Number],
+	'gyroscopeX': [Number],
+	'gyroscopeY': [Number],
+	'gyroscopeZ': [Number],
+	'lightIntensity': Number,
+	'roadQuality': Number
 	// TODO 'roadQuality': Number // rdec = 2, oranzn = 1, zelen = 0 posebi model
+
 });
 
 module.exports = mongoose.model('PhoneData', PhoneDataSchema);
