2c01d4b5e15853475ef64f265fe2d2ac3226ba22
z-seni <35400507+z-seni@users.noreply.github.com>
2023-05-22 20:45:22 +0200

    added algorithm for calculating the shakines

--- backend/controllers/PhoneDataController.js
+++ backend/controllers/PhoneDataController.js
@@ -1,5 +1,41 @@
 var PhonedataModel = require('../models/PhoneDataModel.js');
 
+
+function calculateShakingLevel(accelerometerData, gyroscopeData) {
+    // Calculate standard deviation for accelerometer data
+    const accelerometerStandardDeviation = calculateStandardDeviation(accelerometerData);
+    // Calculate standard deviation for gyroscope data
+    const gyroscopeStandardDeviation = calculateStandardDeviation(gyroscopeData);
+    // Determine shaking level based on standard deviation values
+    if (accelerometerStandardDeviation > 0.5 || gyroscopeStandardDeviation > 0.5) {
+        console.log('High shaking level');
+      return 2;
+    } else if (accelerometerStandardDeviation > 0.3 || gyroscopeStandardDeviation > 0.3) {
+        console.log('Medium shaking level');
+        return 1;
+    } else {
+        console.log('Low shaking level');
+        return 0;
+    }
+  }
+  
+  // Function to calculate the standard deviation of an array of numbers
+  function calculateStandardDeviation(data) {
+    const n = data.length;
+    if (n <= 1) {
+      return 0;
+    }
+    // Calculate the mean
+    const mean = data.reduce((sum, value) => sum + value, 0) / n;
+    // Calculate the sum of squares of differences from the mean
+    const sumOfSquares = data.reduce((sum, value) => sum + Math.pow(value - mean, 2), 0);
+    // Calculate the variance
+    const variance = sumOfSquares / (n - 1);
+    // Calculate the standard deviation
+    const standardDeviation = Math.sqrt(variance);
+    return standardDeviation;
+  }
+
 /**
  * PhoneDataController.js
  *
@@ -47,14 +83,20 @@ module.exports = {
         });
     },
 
+    
+
     /**
      * PhoneDataController.create()
      */
     create: function (req, res) {
         // console.log(req.body.capturedBy);
 
-        // console.log(req.body.accelerometerX);
+        console.log(req.body.accelerometerX);
+        console.log(req.body.accelerometery);
+        console.log(req.body.accelerometerZ);
         // console.log(req.body.longitude_end);
+        
+
         var PhoneData = new PhonedataModel({
             capturedBy : req.body.capturedBy,
             latitude_start : req.body.latitude_start,
@@ -72,6 +114,7 @@ module.exports = {
             gyroscopeY: req.body.gyroscopeY,
             gyroscopeZ: req.body.gyroscopeZ,
             lightIntensity: req.body.lightIntensity,
+            roadQuality: calculateShakingLevel(req.body.accelerometerX, req.body.gyroscopeX)
         });
 
         PhoneData.save(function (err, PhoneData) {
