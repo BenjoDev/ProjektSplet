40bf9c704a01fba0b785451da6aed5cd9ba07621
BenjoDev <benjamintrzan@protonmail.com>
2023-05-25 23:29:30 +0200

    added data refreshing every 10 sec, but map also refreshes

--- frontend/src/components/PhoneData.js
+++ frontend/src/components/PhoneData.js
@@ -1,37 +1,93 @@
 import { useState, useEffect } from 'react';
+import React, { useRef } from 'react';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import PhoneMap from './PhoneMap';
+import 'leaflet-routing-machine';
 
 function PhoneData() {
-    const [phoneData, setPhoneData] = useState([]);
-  
-    useEffect(() => {
-      const getPhoneData = async function() {
-        const res = await fetch("http://localhost:3001/phoneData");
+  const [phoneData, setPhoneData] = useState([]);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    const fetchPhoneData = async () => {
+      console.log("fetch")
+
+      try {
+        const res = await fetch('http://localhost:3001/phoneData');
         const data = await res.json();
         setPhoneData(data);
-      };
-      getPhoneData();
-    }, []);
+      } catch (error) {
+        console.error('Error fetching phone data:', error);
+      }
+    };
 
-    return (
-      <div>
-        {/* <h3>Phone data:</h3> */}
-        {/* <ul>
-          {phoneData.map((item) => (
-            <li key={item._id}>
-              User: {item.capturedBy}, Latitude: {item.latitude}, Longitude: {item.longitude}, Capture date: {item.captureDate}, 
-              accelerometerX: {item.accelerometerX}, accelerometerY: {item.accelerometerY}, accelerometerZ: {item.accelerometerZ}, 
-              userAccelerometerX: {item.userAccelerometerX}, userAccelerometerY: {item.userAccelerometerY}, userAccelerometerZ: {item.userAccelerometerZ}, 
-              gyroscopeX: {item.gyroscopeX}, gyroscopeY: {item.gyroscopeY}, gyroscopeZ: {item.gyroscopeZ}, lightIntensity: {item.lightIntensity}
-            </li>
-          ))}
-        </ul> */}
-        <PhoneMap
-          phoneData={phoneData}
-        />
-      </div>
-    );
-  }
+    fetchPhoneData();
+
+    const interval = setInterval(fetchPhoneData, 10000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    const map = L.map(mapRef.current, {
+      fullscreenControl: true
+    }).setView([0, 0], 3);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18
+    }).addTo(map);
+
+    const updateMap = () => {
+      console.log("map");
+
+      map.eachLayer(layer => {
+      if (!(layer instanceof L.TileLayer)) {
+        map.removeLayer(layer);
+        }
+      });
+
+      phoneData.forEach(item => {
+        let color = 'gray';
+        if (item.roadQuality === 0) {
+          color = 'green';
+        } else if (item.roadQuality === 1) {
+          color = 'orange';
+        } else if (item.roadQuality === 2) {
+          color = 'red';
+        }
+
+        const polyline = L.polyline(
+          [
+            L.latLng(item.latitude_start, item.longitude_start),
+            L.latLng(item.latitude_end, item.longitude_end)
+          ],
+          {
+            color: color
+          }
+        ).addTo(map);
+      });
+    };
+    updateMap();
+
+
+    return () => {
+      if (map && typeof map.remove === 'function') {
+        map.remove();
+      }
+    };
+  }, [phoneData]);
+
+  return (
+    <div>
+      <div style={{ height: '100vh', width: '100vw' }}>
+      <div id="map" ref={mapRef} style={{ height: '100%', width: '100%' }} />
+    </div>
+    </div>
+  );
+}
 
 export default PhoneData;
--- frontend/src/components/PhoneMap.js
+++ frontend/src/components/PhoneMap.js
@@ -1,92 +1,72 @@
-import { useState, useEffect } from 'react';
-import React, { useRef } from 'react';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-routing-machine';
+// import { useState, useEffect } from 'react';
+// import React, { useRef } from 'react';
+// import L from 'leaflet';
+// import 'leaflet/dist/leaflet.css';
+// import 'leaflet-routing-machine';
 
-function PhoneMap({ phoneData }) {
-  const mapRef = useRef(null);
-  const routingControlRefs = useRef([]);
+// function PhoneMap({ phoneData }) {
+//   const mapRef = useRef(null);
+//   const routingControlRefs = useRef([]);
 
-  useEffect(() => {
-    const map = L.map(mapRef.current, {
-      fullscreenControl: true
-    }).setView([0, 0], 3);
+//   useEffect(() => {
+//     const map = L.map(mapRef.current, {
+//       fullscreenControl: true
+//     }).setView([0, 0], 3);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      maxZoom: 18
-    }).addTo(map);
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//       attribution:
+//         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+//       maxZoom: 18
+//     }).addTo(map);
 
-    phoneData.forEach(item => {
-      let color = 'gray'; 
-      if (item.roadQuality === 0) {
-        color = 'green';
-      }
-      else if (item.roadQuality === 1){
-        color = 'orange'; 
-      }
-      else if (item.roadQuality === 2){
-        color = 'red';
-      }
+//     const updateMap = () => {
+//       phoneData.forEach(item => {
+//         let color = 'gray';
+//         if (item.roadQuality === 0) {
+//           color = 'green';
+//         } else if (item.roadQuality === 1) {
+//           color = 'orange';
+//         } else if (item.roadQuality === 2) {
+//           color = 'red';
+//         }
 
-      const polyline = L.polyline([
-        L.latLng(item.latitude_start, item.longitude_start), 
-        L.latLng(item.latitude_end, item.longitude_end)], 
-        { 
-          color: color 
-        }).addTo(map);
+//         const polyline = L.polyline(
+//           [
+//             L.latLng(item.latitude_start, item.longitude_start),
+//             L.latLng(item.latitude_end, item.longitude_end)
+//           ],
+//           {
+//             color: color
+//           }
+//         ).addTo(map);
+//       });
+//     };
+//     updateMap();
 
+//     const interval = setInterval(() => {
+//       map.eachLayer(layer => {
+//         if (!(layer instanceof L.TileLayer)) {
+//           map.removeLayer(layer);
+//         }
+//       });
 
+//       updateMap();
+//     }, 10000);
 
-      // const routingControl = L.Routing.control({
-      //   waypoints: [
-      //     L.latLng(item.latitude_start, item.longitude_start),
-      //     // L.latLng(middleLocation.latitude, middleLocation.longitude),
-      //     L.latLng(item.latitude_end, item.longitude_end)
-      //   ],
-        
-      //   routeWhileDragging: true,
-      //   lineOptions: {
-      //     styles: [
-      //       {
-              
-      //         color: color,
-      //         opacity: 0.6,
-      //         weight: 4
-      //       }
-      //     ]
-      //   },
-      //   createMarker: function() { return null; }
+//     return () => {
+//       clearInterval(interval);
 
-      // }).addTo(map);
+//       if (map && typeof map.remove === 'function') {
+//         map.remove();
+//       }
+//     };
+//   }, [phoneData]);
 
-      // routingControlRefs.current.push(routingControl);
+//   return (
+//     <div style={{ height: '100vh', width: '100vw' }}>
+//       <div id="map" ref={mapRef} style={{ height: '100%', width: '100%' }} />
+//     </div>
+//   );
+// }
 
-    });
-
-
-    return () => {
-      // routingControlRefs.current.forEach((routingControlRef) => {
-      //   if (routingControlRef) {
-      //     routingControlRef.getPlan().setWaypoints([]);
-      //     routingControlRef.hide();
-      //     map.removeControl(routingControlRef);
-      //   }
-      // });
-
-      if (map && typeof map.remove === 'function') {
-        map.remove();
-      }
-    };
-  }, [phoneData]);
-
-  return (
-    <div style={{ height: '100vh', width: '100vw' }}>
-      <div id="map" ref={mapRef} style={{ height: '100%', width: '100%' }} />
-    </div>
-  );
-}
-
-export default PhoneMap;
+// export default PhoneMap;
