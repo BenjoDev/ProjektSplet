016f94194f6c364c7af9f8b0b4a1e565965bc2f0
BenjoDev <benjamintrzan@protonmail.com>
2023-05-31 13:49:04 +0200

    small quick changes

--- Makefile
+++ Makefile
@@ -1,11 +1,9 @@
 build:
 	cd backend && $(MAKE) build
 	cd frontend && $(MAKE) build
-	cd ../ProjektVid && $(MAKE) build
 
 run:
 	docker-compose up 
-	# & cd ../ProjektVid && flask run
 
 stop:
 	docker-compose down
--- run.bat
+++ run.bat
@@ -1,4 +1,7 @@
 start cmd.exe /k "bash ./run_linux.sh"
 
-cd ..\ProjektVid
-c:\Users\zanse\Anaconda3\Scripts\activate base && flask run --host=0.0.0.0
+@REM cd ..\ProjektVid
+@REM c:\Users\zanse\Anaconda3\Scripts\activate base && flask run --host=0.0.0.0
+
+cd ..\..\projekt_vid
+c:\Users\benja\Anaconda3\Scripts\activate N1 && flask run --host=0.0.0.0
